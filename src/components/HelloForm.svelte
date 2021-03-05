deea4767ed41bf571aca6d285321663be361875e
a <a@b.c>
2021-03-05 16:53:34 +0300

    Svelte reactive expression and bind value

---
+++ src/components/HelloForm.svelte
@@ -0,0 +1,21 @@
+<h4>Tell Us About Yourself</h4>
+
+<p>Hello {fullName},</p>
+<form action="">
+    <div>
+        <label for="firstName">First Name</label>
+        <input type="text" id="firstName" bind:value="{firstName}">
+    </div>
+
+    <div>
+        <label for="lastName">Last Name</label>
+        <input type="text" id="lastName" bind:value="{lastName}">
+    </div>
+</form>
+
+<script>
+    let firstName = '';
+    let lastName = ''
+
+    $: fullName = `${firstName} ${lastName}`
+</script>
